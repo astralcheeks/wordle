0acc979aee750858307cdbdc1da9dbe848d73cdc
astralcheeks <98672834+astralcheeks@users.noreply.github.com>
2022-01-31 14:54:27 -0500

    Update unlimited_wordle.py

--- unlimited_wordle.py
+++ unlimited_wordle.py
@@ -5,30 +5,30 @@ from termcolor import colored
 
 allowed_attempts = 6
 word_length = 5
-word_bank = [word.upper() for word in brown.words() if len(word) ==word_length and word.isalpha()]
+word_bank = [word.upper() for word in brown.words() if len(word) == word_length and word.isalpha()]
 
-def validate(attempt, answer, current_dict):
+def validate(attempt, answer, current_alpha):
     result = ''
-    for pos, char in enumerate(attempt): # Iterates over entry to check if letter is correct
+    for pos, char in enumerate(attempt):
         if char == list(answer)[pos]: # Letter is correct and in right position
             result += (' ' + colored(char, 'green') + ' ')
-            current_dict = current_dict.replace(char, (colored(char, 'green')))
+            current_alpha = current_alpha.replace(char, (colored(char, 'green')))
         elif char in answer: # Letter is correct but in wrong position
             result += (' ' + colored(char, 'yellow') + ' ')
-            current_dict = current_dict.replace(char, (colored(char, 'yellow')))
+            current_alpha = current_alpha.replace(char, (colored(char, 'yellow')))
         else: # Letter is incorrect
             result += (' ' + char + ' ')
-            current_dict = current_dict.replace(char, (colored(char, 'red')))  
+            current_alpha = current_alpha.replace(char, (colored(char, 'red')))  
     print('\n' + result + '\n')
-    return current_dict
+    return current_alpha
 
 def guess(answer):
-    dict_words = ('\n\n   Q  W  E  R  T  Y  U  I  O  P   \n    A  S  D  F  G  H  J  K  L    \n      Z  X  C  V  B  N  M     \n\n')
-    print('\nEnter your 6 guesses. \n')
-    for i in range(allowed_attempts): # Iterates over 6 attempts
+    alphabet = ('\n\n   Q  W  E  R  T  Y  U  I  O  P   \n    A  S  D  F  G  H  J  K  L    \n      Z  X  C  V  B  N  M     \n\n')
+    print('\nEnter your ' + str(allowed_attempts) + ' guesses. \n')
+    for i in range(allowed_attempts):
         while True:
-            print(dict_words)
-            attempt = input('Attempt #' + str(i + 1) + ': ').upper() # Takes user input
+            print(alphabet)
+            attempt = input('Attempt #' + str(i + 1) + ': ').upper()
             if len(attempt) == word_length and attempt in word_bank: # Ensures user input is valid
                 break
             else:
@@ -36,16 +36,16 @@ def guess(answer):
         if attempt == answer: # Checks if user won
             return True
         else:
-            dict_words = validate(attempt, answer, dict_words) # Calls validation function
+            alphabet = validate(attempt, answer, alphabet)
     return False
     
 def main():
-    print('\n\n\nI am thinking of a 5-letter word. Can you guess in 6 tries? \n')
+    print('\n\n\nI am thinking of a ' + str(word_length) + '-letter word. Can you guess in ' + str(allowed_attempts) + ' tries? \n')
     print('If a letter is in the right place, it will be ' + colored('green', 'green') + '.\n' +
     'If it is in the word but in the wrong place, it will be ' + colored('yellow', 'yellow') + '.\n' +
     'If it is not in the word, it will be ' + colored('red', 'red') + '.\n')
     while True:
-        answer = random.choice(word_bank) # Picks a 5-letter word for this turn
+        answer = random.choice(word_bank) # Picks a word for this turn
         if guess(answer):
             print(colored('\nCongratulations! You won!\n', 'green', attrs=['bold']))
         else:
